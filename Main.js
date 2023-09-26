03a3f021bbdb31c2dcefcf420e3c55f59b69064e
Anton Zhukov <anton21120@mail.ru>
2023-09-26 19:12:48 +0300

    if if

--- Main.js
+++ Main.js
@@ -1,5 +1,9 @@
-if (){
+const age = 7
 
-} else {
-  
+if (age > 18){
+  console.log('Is adult')
+} if (age >= 12 && age <= 18){
+  console.log('Is teenager')
+} if (age < 12){
+  console.log('Is child')
 }
