9d89653e76b9e4cbc27c4076ac4e5280c4d6e6c5
Anton Zhukov <anton21120@mail.ru>
2023-10-02 19:26:11 +0300

    none chikl

--- Main.js
+++ Main.js
@@ -1,6 +1,11 @@
-let value = 11
-console.log(value >= 0 ? value : -value)
-
-value = - 5
-const res = value >= 0 ? value : - value
-console.log(res)
+let i = 0
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
+console.log(i)
+i++
