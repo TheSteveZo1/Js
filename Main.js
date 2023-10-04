417c9eac1b6e3030ee9fa4cca93fb8cfddcc1286
Anton Zhukov <anton21120@mail.ru>
2023-10-04 17:59:09 +0300

    for in

--- Main.js
+++ Main.js
@@ -1,9 +1,5 @@
-const myObject = {
-  x: 10,
-  y: true,
-  z: 'abc'
-}
+const myArray = [true, 10, 'abc', null]
 
-for (const key in myObject) {
-  console.log(key, myObject[key])
+for (const key in myArray) {
+  console.log(myArray[key])
 }
