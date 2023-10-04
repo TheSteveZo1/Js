1af74e0e57317d6e7410ce493c8a03f09b69cf1f
Anton Zhukov <anton21120@mail.ru>
2023-10-04 18:43:33 +0300

    export/import

--- Main.js
+++
@@ -1,5 +0,0 @@
-const myArray = [true, 10, 'abc', null]
-
-for (const element of myArray) {
-  console.log(element)
-}
---
+++ modules/moduletwo.mjs
@@ -0,0 +1,7 @@
+import sum from './moduleone.mjs'
+
+const res1 = sum(10, 2)
+console.log(res1)
+
+const res2 = sum(5, 10)
+console.log(res2)
