021a4f7df8d3ed3e38be1da8d4890d6422ff71cc
Anton Zhukov <anton21120@mail.ru>
2023-10-12 21:04:04 +0300

    /

--- modules/moduletwo.mjs
+++ modules/moduletwo.mjs
@@ -1,16 +1,6 @@
-class Comment {
-  constructor(text) {
-    this.text = text
-    this.votesQty = 0
-  }
-  upvote() {
-    this.votesQty += 1
-  }
-}
-
 const firstComment = new Comment('First comment')
 
-firstComment.upvote()
-console.log(firstComment.votesQty)
-firstComment.upvote()
-console.log(firstComment.votesQty)
+firstComment.hasOwnProperty('text')
+firstComment.hasOwnProperty('votesQty')
+firstComment.hasOwnProperty('upvote')
+firstComment.hasOwnProperty('hasOwnProperty')
