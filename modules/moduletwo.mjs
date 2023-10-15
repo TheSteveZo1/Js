9fc83732b892277e6b9df5cb60bb8bb2396c9cbb
Anton Zhukov <anton21120@mail.ru>
2023-10-15 22:07:55 +0300

    .

--- modules/moduletwo.mjs
+++ modules/moduletwo.mjs
@@ -1,15 +1,8 @@
-class Comment {
-  constructor(text) {
-    this.text = text
-    this.votesQty = 0
-  }
-
-  upvote() {
-    this.votesQty += 1
-  }
-  static mergeComments(first, second){
-    return `${first} ${second}`
+class NumbersArray extends Array {
+  sum(){
+    return this.reduce ((el,acc) => acc += el, 0)
   }
 }
-
-Comment.mergeComments ('First comment.', 'Second comment.')
+const myArray = new NumbersArray(2, 5, 7)
+console.log(myArray)
+myArray.sum()
