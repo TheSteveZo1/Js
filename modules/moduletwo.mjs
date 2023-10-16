0f5a79a014a6867c8b541628c6cf2a48507d58b6
Anton Zhukov <anton21120@mail.ru>
2023-10-16 20:51:26 +0300

    new promise

--- modules/moduletwo.mjs
+++ modules/moduletwo.mjs
@@ -1,8 +1,3 @@
-class NumbersArray extends Array {
-  sum(){
-    return this.reduce ((el,acc) => acc += el, 0)
-  }
-}
-const myArray = new NumbersArray(2, 5, 7)
-console.log(myArray)
-myArray.sum()
+const myPromise = new Promise((resolve, reject)=> {
+  
+})
