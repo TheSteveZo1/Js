db25947423b1e5863ad1b8490d3bf8325bea2c1a
Anton Zhukov <anton21120@mail.ru>
2023-10-17 20:36:59 +0300

    .

--- modules/moduletwo.mjs
+++ modules/moduletwo.mjs
@@ -1,4 +1,11 @@
-fetch('https://jsonplaceholder.typicode.com/todos')
+const getData = (url) =>
+  new Promise((resolve, reject)=>
+  fetch(url)
   .then(response => response.json())
-  .then(json => console.log(json))
-  .catch(error => console.error(error))
+  .then(json => resolve(json))
+  .catch(error => reject(error))
+  )
+
+  getData('https://jsonplaceholder.typicode.com/todos')
+  .then(data => console.log(data))
+  .catch(error => console.log(error.message))
