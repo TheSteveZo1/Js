889841e6340f6c739d0743c41d1d7f4e166f2419
Anton Zhukov <anton21120@mail.ru>
2023-10-17 20:46:34 +0300

    ;

--- Main.js
+++ Main.js
@@ -1,7 +1,7 @@
-async function asyncFn() {
-
+const asyncFn = async () => {
+  throw new Error('There was an error!')
 }
 
-const asyncFn = async() => {
-
-}
+asyncFn()
+  .then(value => console.log(value))
+  .catch(error => console.log(error.message))
