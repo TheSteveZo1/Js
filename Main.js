ed28350f1ee756e57d010b20288ccceba6a1bf8d
Anton Zhukov <anton21120@mail.ru>
2023-10-19 21:13:56 +0300

    .

--- Main.js
+++ Main.js
@@ -1,11 +1,12 @@
-const timerPromise = () =>
-  new Promise((resolve, reject) =>
-    setTimeout(() => resolve(), 2000))
-
-const asyncFn = async () => {
-  console.log('Timer starts')
-  await timerPromise()
-  console.log('Timer ended')
+const getData = async (url) => {
+  const res = await fetch(url)
+  const json = await res.json()
+  return json
 }
-
-asyncFn()
+const url = 'https://jsonplaceholder.typicode.com/todos'
+try{
+const data = await getData(url)
+console.log(data)
+} catch (error) {
+  console.log(error.message)
+} 
