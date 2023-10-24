31ff6ca3e226c7153554ccc470200aeb1e4865b2
Anton Zhukov <anton21120@mail.ru>
2023-10-24 19:41:05 +0300

    s

--- Main.js
+++ Main.js
@@ -1,12 +1,6 @@
-const getData = async (url) => {
-  const res = await fetch(url)
-  const json = await res.json()
-  return json
-}
-const url = 'https://jsonplaceholder.typicode.com/todos'
-try{
-const data = await getData(url)
-console.log(data)
-} catch (error) {
-  console.log(error.message)
-} a/
+let admin, name
+
+
+name = 'jhon'
+admin = 'jhon'
+console.log (admin)
